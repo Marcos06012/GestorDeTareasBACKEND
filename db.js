1abc760929e4259c30ec37199c0c7de07cd22b8e
MARCOS JARED ALAS MORALES <marcos.alas24@itca.edu.sv>
2026-05-28 15:15:09 -0600

    Fix: implementando exportacion doble en db.js

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
+
 const db = mysql.createPool({
   host: process.env.MYSQLHOST || process.env.DB_HOST,
   user: process.env.MYSQLUSER || process.env.DB_USER,
   password: process.env.MYSQLPASSWORD || process.env.MYSQL_ROOT_PASSWORD || process.env.DB_PASSWORD,
   database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || process.env.DB_NAME,
   port: process.env.MYSQLPORT ? parseInt(process.env.MYSQLPORT) : 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-export default db;
+// EXPORTACIÓN DOBLE: Esto blinda el archivo para que funcione con cualquier tipo de importación
+export { db }; // Soporta: import { db } from "../db.js"
+export default db; // Soporta: import db from "../db.js"
